c505b725ff23e7c13ea113da479b91b3d8382f59
rustyzzz <lucasfpv21@gmail.com>
2026-06-13 12:05:19 +0200

    entity data file name change, pellet data class added

---
+++ src/entity_data.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel, Field
+
+
+class PlayerData(BaseModel):
+
+    name: str = Field(min_length=3,
+                      max_length=10,
+                      pattern=r"^[a-zA-Z0-9 ]+$",
+                      default="   ")
+
+    score: int = Field(ge=0, default=0)
+
+    lives: int = Field(gt=0, default=3)
+
+
+class GhostData(BaseModel):
+
+    vunerable: bool = Field(default=False)
+
+
+class PelletData(BaseModel):
+
+    power: bool = Field(default=False)
