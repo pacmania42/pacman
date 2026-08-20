468e51a4c4ab78a9ed32a4b544537c4787261262
Semere Meharena Mebrahtom <semere.meharena1@gmail.com>
2026-08-20 22:39:51 +0200

    Error in generation properly caught and gracefully exits

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from src.adapter import AdapterError
 from src.config import Parser, ParserError
 from src.models import Level
 
@@ -7,13 +8,17 @@ def main() -> None:
         config = Parser().get_config()
         levels: list[Level] = []
         for rank, lvl in enumerate(config.levels):
-            levels.append(
-                Level(
-                    rank=rank,
-                    width=lvl.width,
-                    height=lvl.height,
+            try:
+                levels.append(
+                    Level(
+                        rank=rank,
+                        width=lvl.width,
+                        height=lvl.height,
+                    )
                 )
-            )
+            except AdapterError as e:
+                print(e)
+                return
 
             lv = levels[-1]
             print(lv.rank, lv.width, lv.height, lv.seed, lv.generator)
