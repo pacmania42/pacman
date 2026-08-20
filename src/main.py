ebf1122ef6aa2aa95bbc41142dd0b7622e10cfec
Semere M. Mebrahtom <88221834+semere-meb@users.noreply.github.com>
2026-08-20 23:11:11 +0200

    created empty window (#86)

--- src/main.py
+++ src/main.py
@@ -1,27 +1,31 @@
 from src.adapter import AdapterError
 from src.config import Parser, ParserError
 from src.models import Level
+from src.window import Window
 
 
 def main() -> None:
     try:
         config = Parser().get_config()
-        levels: list[Level] = []
-        for rank, lvl in enumerate(config.levels):
-            try:
-                levels.append(
-                    Level(
-                        rank=rank,
-                        width=lvl.width,
-                        height=lvl.height,
-                    )
-                )
-            except AdapterError as e:
-                print(e)
-                return
-
-            lv = levels[-1]
-            print(lv.rank, lv.width, lv.height, lv.seed, lv.generator)
     except ParserError as e:
         print(e)
         return
+    levels: list[Level] = []
+    for rank, lvl in enumerate(config.levels):
+        try:
+            levels.append(
+                Level(
+                    rank=rank,
+                    width=lvl.width,
+                    height=lvl.height,
+                )
+            )
+        except AdapterError as e:
+            print(e)
+            return
+
+        lv = levels[-1]
+        print(lv.rank, lv.width, lv.height, lv.seed, lv.generator)
+
+    win = Window()
+    win.run()
