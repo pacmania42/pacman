1d5b3b306c32ff0a6186559d4ac171412a07bf02
Semere Meharena Mebrahtom <semere.meharena1@gmail.com>
2026-09-04 22:38:36 +0200

    stp-00: basic mlx window created

--- src/main.py
+++ src/main.py
@@ -1,2 +1,9 @@
+from mlx import mlx
+
+
 def main() -> None:
-    print("Hello from pacman")
+    m = mlx.Mlx()
+    mlx_ptr = m.mlx_init()
+    win_ptr = m.mlx_new_window(mlx_ptr, 600, 400, "New Window")
+    m.mlx_hook(win_ptr, 0x21, 0, lambda _: m.mlx_loop_exit(mlx_ptr), None)
+    m.mlx_loop(mlx_ptr)
