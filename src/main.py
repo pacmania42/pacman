6401e95c4c60fe4fdde188b86b8b4529733af3e5
sosias <38495175+sosias@users.noreply.github.com>
2026-09-25 14:56:57 +0200

    bug-05: general mazegen package error. we threat mazegen as a blackbox, so each error is raised we should handle it gracefully

--- src/main.py
+++ src/main.py
@@ -8,6 +8,7 @@ from src.core.input import EventBuffer, InputTracker
 from src.core.scene_stack import SceneStack
 from src.core.settings import Settings
 from src.core.window import Window
+from src.entities.maze import MazeError
 
 
 class Game:
@@ -60,7 +61,11 @@ class Game:
         now = time.monotonic()
         if now >= self.last_tick + Settings.tick:
             self.last_tick = now
-            self.scenes.update(self.input.begin_frame())
+            try:
+                self.scenes.update(self.input.begin_frame())
+            except MazeError as e:
+                print(f"Error: {e}")
+                self.scenes.quit()
             if self.scenes.should_quit:
                 self.window.exit(None)
                 return
